ced9817f8fb6dc20cdc12069ed8ab79f321dd4b5
AayushSheth9 <163661435+AayushSheth9@users.noreply.github.com>
2025-04-11 15:59:04 +0530

    group schema

---
+++ server/models/group.js
@@ -0,0 +1,28 @@
+const mongoose=require('mongoose');
+
+const groupSchema= new mongoose.Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  members:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }],
+  transactions:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'Expense',
+  }],
+  createdBy:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt:{
+    type: Date,
+    default: Date.now
+  }
+});
+const Group= mongoose.model('Group',groupSchema);
+module.exports=Group;
