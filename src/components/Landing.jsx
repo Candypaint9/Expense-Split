089095252cc75b350d9e5ddc5a5e730378ff118e
Candypaint9 <advaitdeshmukh999@gmail.com>
2025-05-08 16:33:39 +0530

    fixed missing import

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FiPlus } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 function Landing({ userData }) {
