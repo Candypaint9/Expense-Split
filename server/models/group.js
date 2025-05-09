9f1307917b13ca7065d816145e7d8a06d567b21b
Candypaint9 <advaitdeshmukh999@gmail.com>
2025-05-09 16:46:47 +0530

    adding expense probably works

--- server/models/group.js
+++ server/models/group.js
@@ -40,6 +40,37 @@ groupSchema.methods.isMember = function (userId) {
 //   // Implementation would be in the controller for more complex logic
 // };
 
-const Group = mongoose.model('Group', groupSchema);
+// Add a method to update balances
+groupSchema.methods.updateBalances = function (paidBy, shares) {
+    if (!this.balances) {
+        this.balances = new Map();
+    }
 
+    for (const [debtorId, amount] of Object.entries(shares)) {
+        if (debtorId === paidBy) continue;
+
+        // Initialize maps if not already present
+        if (!this.balances.has(debtorId)) {
+            this.balances.set(debtorId, new Map());
+        }
+        if (!this.balances.has(paidBy)) {
+            this.balances.set(paidBy, new Map());
+        }
+
+        const debtorMap = this.balances.get(debtorId);
+        const creditorMap = this.balances.get(paidBy);
+
+        // Add debt
+        debtorMap.set(paidBy, (debtorMap.get(paidBy) || 0) + amount);
+
+        // Net reverse (optional)
+        creditorMap.set(debtorId, (creditorMap.get(debtorId) || 0) - amount);
+    }
+
+    // Tell Mongoose the nested map was modified
+    this.markModified('balances');
+};
+
+
+const Group = mongoose.model('Group', groupSchema);
 export default Group;
